bd2f3885c257a70ec141e4d6518a8a4c59aeadef
suhail <suhail609@gmail.com>
2025-03-23 12:34:52 +0400

    feat(web/sidebar): hightlight selected chat tab

--- apps/web/app/chat/Sidebar.tsx
+++ apps/web/app/chat/Sidebar.tsx
@@ -12,7 +12,9 @@ import { RootState } from "../../redux/store";
 import { setSelectedChat } from "../../redux/chatHistorySlice";
 
 const Sidebar = () => {
-  const { chats } = useSelector((state: RootState) => state.chatHistory);
+  const { chats, selectedChatId } = useSelector(
+    (state: RootState) => state.chatHistory
+  );
   const dispatch = useDispatch();
 
   const handleSelectChat = (selectedChatId: string) => {
@@ -33,12 +35,14 @@ const Sidebar = () => {
                 onClick={() => handleSelectChat(chat.id)}
                 key={index}
                 //TODO: highlight if this is the selected chat
-                className="flex py-3 px-3 items-center gap-3 relative rounded-md hover:bg-[#2A2B32] cursor-pointer break-all hover:pr-4 group"
+                className={`flex py-3 px-3 items-center gap-3 relative rounded-md hover:bg-[#2A2B32] ${chat.id === selectedChatId ? "bg-[#2A2B32]" : ""} cursor-pointer break-all hover:pr-4 group`}
               >
                 <FiMessageSquare className="h-4 w-4" />
                 <div className="flex-1 text-ellipsis max-h-5 overflow-hidden break-all relative">
                   {chat.title}
-                  <div className="absolute inset-y-0 right-0 w-8 z-10 bg-gradient-to-l from-gray-900 group-hover:from-[#2A2B32]"></div>
+                  <div
+                    className={`absolute inset-y-0 right-0 w-8 z-10 bg-gradient-to-l from-gray-900 group-hover:from-[#2A2B32] ${chat.id === selectedChatId ? "from-[#2A2B32]" : ""}`}
+                  ></div>
                 </div>
               </a>
             ))}
